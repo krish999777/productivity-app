dba2fa2692ce5c51913745b24f829c8c998f832e
Krish Shah <krishcshah09@gmail.com>
2026-04-10 16:09:33 +0530

    merged files

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,3 @@
-import {display} from './render.js'
-import {getState,addTask,deleteTask,clearTask,toggleStatus,currentFilter,changeFilter,changeEdit,getEdit,editText,cancelEdit,assignEdit} from './state.js'
 const allBtn=document.getElementById('all-btn')
 const completedBtn=document.getElementById('completed-btn')
 const pendingBtn=document.getElementById('pending-btn')
@@ -52,3 +50,94 @@ submitBtn.addEventListener('click',()=>{
     count++
     inputField.value=""
 })
+const listEl=document.getElementById('tasks')
+function display(){
+    let tasks=[]
+    const unfiltered=getState()
+    const filter=currentFilter()
+    const editId=getEdit()
+    if(filter=='all'){
+        tasks=unfiltered
+    }
+    else if(filter=='completed')
+        tasks= unfiltered.filter(task=>task.status)
+    else
+        tasks= unfiltered.filter(task=>!task.status)
+    listEl.innerHTML=tasks.map(task=>{
+        if(task.id===editId){
+            return(`<li element-task-id="${task.id}"><input class="edit-input"id="edit-input-field" type="text" value="${getCurrentEdit()}">
+                    <div class="tasks-buttons"><button class="delete" id="cancel-btn">Cancel</button><button class="save" id="save-btn">Save</button></div>
+                    </li>`)
+        }
+        return( `<li element-task-id="${task.id}"><div class="title-check"><div class="task-title">${task.title}</div>
+                <input class="tasks-checkbox" type="checkbox" id="checkbox" ${task.status?'checked':''}></input></div>
+                <div class="task-status">Status:${task.status?'✅':'❌'}</div>
+                <div class="tasks-buttons"><button class="delete" id="delete-btn">Delete</button><button class="edit" id="edit-btn">Edit</button></div>
+                </li>`) 
+    }).join(' ')
+}
+let tasks=[]
+let filter='all'
+let edit=null
+let currentEditText=""
+function getEdit(){
+    return edit
+}
+function changeEdit(id){
+    edit=Number(id)===0?null:Number(id)
+    tasks.forEach(task=>{
+        if(task.id===edit){
+            currentEditText=task.title
+        }
+    })
+}
+function getCurrentEdit(){
+    return currentEditText
+}
+function editText(current){
+    currentEditText=current
+}
+function cancelEdit(){
+    currentEditText=""
+    edit=null
+}
+function assignEdit(){
+    const editId=getEdit()
+    tasks=tasks.map(task=>{
+        if(task.id===editId){
+            return {id:task.id,title:currentEditText,status:task.status}
+        }
+        return task
+    })
+    edit=null
+}
+function currentFilter(){
+    return filter
+}
+function changeFilter(type){
+    if(type!='all'&&type!='completed'&&type!='pending'){
+        throw new error('Invalid Filter')
+    }
+    filter=type
+}
+function getState(){
+    return [...tasks]
+}
+function addTask(id,title,status){
+    tasks=[...tasks,{id,title,status}]
+}
+function deleteTask(taskId){
+    tasks=tasks.filter(obj=>obj.id!=taskId)
+}
+function clearTask(){
+    tasks=[]
+}
+function toggleStatus(id,state){
+    tasks=tasks.map((task)=>{
+        if(task.id===Number(id))
+        {
+            return {id:Number(id),title:task.title,status:state}
+        }
+        return task
+    })
+}
